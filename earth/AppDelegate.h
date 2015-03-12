950b374fb2272f36a03fd71ec9721a7729b8f249
ChenFeicun <276549366@qq.com>
2015-03-12 09:11:06 +0800

    Initial Commit

---
+++ earth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  earth
+//
+//  Created by Feicun on 15/3/12.
+//  Copyright (c) 2015年 ShuXiaJian.Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ earth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  earth
+//
+//  Created by Feicun on 15/3/12.
+//  Copyright (c) 2015年 ShuXiaJian.Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
